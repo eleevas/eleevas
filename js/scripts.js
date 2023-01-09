04143015a236b7f06c6265c154dee004ff454441
eleevas <thor.dw@gmail.com>
2023-01-09 03:36:59 +0100

    Pushing v2 out

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,50 @@
 $(document).ready(function() {
+  // const currentYear = new Date().getFullYear();
+  const copyright = `&copy; ${new Date().getFullYear()}.`;
+  document.getElementById('copyright').innerHTML = copyright;
 
+  // G4A
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-Q4PBJF9K2V');
 });
 
-// check for saved 'darkSide' in localStorage
-let darkSide = localStorage.getItem('darkSide'); 
+// check for saved 'lightSide' in localStorage
+let lightSide = localStorage.getItem('lightSide'); 
 
 const darkSideToggle = document.querySelector('#dark-side-toggle');
 
-const enableDarkSide = () => {
+const enableLightSide = () => {
   // 1. Add the class to the body
-  document.body.classList.add('darkside');
+  document.body.classList.add('lightside');
   // 2. Update darkSide in localStorage
-  localStorage.setItem('darkSide', 'enabled');
+  localStorage.setItem('lightSide', 'enabled');
 }
 
-const disableDarkSide = () => {
+const disableLightSide = () => {
   // 1. Remove the class from the body
-  document.body.classList.remove('darkside');
-  // 2. Update darkSide in localStorage 
-  localStorage.setItem('darkSide', null);
+  document.body.classList.remove('lightside');
+  // 2. Update lightSide in localStorage 
+  localStorage.setItem('lightSide', null);
 }
  
-// If the user already visited and enabled darkSide
+// If the user already visited and enabled lightSide
 // start things off with it on
-if (darkSide === 'enabled') {
-  enableDarkSide();
+if (lightSide === 'enabled') {
+  enableLightSide();
 }
 
 // When someone clicks the button
 darkSideToggle.addEventListener('click', () => {
-  // get their darkSide setting
-  darkSide = localStorage.getItem('darkSide'); 
+  // get their lightSide setting
+  lightSide = localStorage.getItem('lightSide'); 
   
   // if it not current enabled, enable it
-  if (darkSide !== 'enabled') {
-    enableDarkSide();
+  if (lightSide !== 'enabled') {
+    enableLightSide();
   // if it has been enabled, turn it off  
   } else {  
-    disableDarkSide(); 
+    disableLightSide(); 
   }
 });
